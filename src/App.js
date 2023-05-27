08fd52d4d1b94c913bf133d03b2f1b3278063733
jeevva <harun24896@gmail.com>
2023-05-27 12:47:52 +0700

    tes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+
+import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Apps">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
